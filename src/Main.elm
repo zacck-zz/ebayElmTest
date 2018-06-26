f2d95a4f4f3262e12783347ecfe9c92a5a5af83a
Zacck Osiemo <coderv63@gmail.com>
2018-06-26 10:53:03 +0200

    add and test listener for terms

--- src/Main.elm
+++ src/Main.elm
@@ -6,24 +6,40 @@ and set up our application
 
 import Html exposing(Html, div, text, button, input)
 import Html.Attributes exposing(..)
+import Html.Events exposing(onClick, onInput)
+
 
 
 
 -- Build a type for our model
 type alias Model =
-  {url : String
+  { q: String
+  , price: String
+  , priceCurrency: String
+  , accountType: String
+  }
+
+-- Build an inital model to start up with
+initialModel : Model
+initialModel =
+  { q = ""
+  , price = ""
+  , priceCurrency = ""
+  , accountType = ""
   }
 
 {-| We use this function to initialize our application when starting up
 We can always start up by checking the value of the url
 -}
 init : (Model, Cmd Msg)
-init = ({ url = "init" }, Cmd.none)
+init = (initialModel, Cmd.none)
 
 
 -- Build some messages to update the state
 type Msg
     = CheckUrl
+    | SetTerm String
+
 
 {-| Use this function to update the state of out form
 We send updates to this function containing a message and the payload
@@ -33,17 +49,21 @@ update msg model =
   case msg of
     CheckUrl ->
       (model, Cmd.none)
+    SetTerm term ->
+      ({ model | q = term }, Cmd.none)
+
+
 
 
 {-| This function contains the view we display for the form it render the UI
 from whatever the state of the model is
 -}
-view : Model -> Html msg
+view : Model -> Html Msg
 view model =
   div [ align "center"]
   [ div []
     [ text "Search"
-    , input [ type_ "text", placeholder "Name"] []
+    , input [ type_ "text", placeholder "iPhone...", onInput SetTerm] []
     ]
   , div []
     [ text "Seller Type"
@@ -57,7 +77,7 @@ view model =
         ]
       , div []
         [ text "Max"
-        , input [ type_ "text", placeholder "500"] []
+        , input [ type_ "text", placeholder "500" ] []
         ]
       , div []
         [ text "Currency"
--- tests/Suite.elm
+++ tests/Suite.elm
@@ -1,14 +1,43 @@
 module Suite exposing (..)
 
 import Expect exposing (Expectation)
-import Fuzz exposing (Fuzzer, int, list, string)
+import Main exposing(..)
 import Test exposing (..)
+
+
+model : Model
+model =
+  { q = ""
+  , price = ""
+  , priceCurrency = ""
+  , accountType = ""
+  }
+
+modelTest : Test
+modelTest =
+  describe "update"
+      [ describe "SetTerm"
+          [ test "adds a term to the state" <|
+              \_ ->
+                  model
+                      |> update (SetTerm "qq")
+                      |> Tuple.first
+                      |> Expect.equal
+                             { q = "qq"
+                              , price = ""
+                              , priceCurrency = ""
+                              , accountType = ""
+                             }
+
+          ]
+      ]
 
 
 suite : Test
 suite =
-    describe "Math tests"
+    describe "Tests"
       [ test "Addition works" <|
         \() ->
           4 |> Expect.equal (3 + 1)
+      , modelTest
       ]
